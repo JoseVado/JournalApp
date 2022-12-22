2287b2ae3baa74ba4e425536dd18438ad8032b9a
vado <86899022+JoseVado@users.noreply.github.com>
2022-12-21 21:07:40 -0600

    terminar login, registro y logut

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -40,7 +40,8 @@ export const LoginPage = () => {
 
   return (
     <AuthLayout title="Login">
-      <form onSubmit={ onSubmit } className='animate__animated animate__fadeIn animate__faster'>
+      <form onSubmit={onSubmit}
+        className='animate__animated animate__fadeIn animate__faster'>
           <Grid container>
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
               <TextField 
